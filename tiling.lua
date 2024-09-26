777c997d89efd5bc6b58a6183e4ae16215d10d2e
Henry Li <yrnehli@proton.me>
2024-09-26 18:11:14 +0100

    cleanup

--- tiling.lua
+++ tiling.lua
@@ -12,31 +12,30 @@ end
 
 function shrink()
     local win = hs.window.focusedWindow()
-    local f = win:frame()
     local screen = win:screen()
+    local frame = win:frame()
     local max = screen:frame()
 
-    f.w = max.w / 1.5
-    f.h = max.h / 1.5
+    frame.w = max.w / 1.5
+    frame.h = max.h / 1.5
 
-    f.x = max.x + (max.w / 2) - (f.w / 2)
-    f.y = max.y + (max.h / 2) - (f.h / 2)
+    frame.x = max.x + (max.w / 2) - (frame.w / 2)
+    frame.y = max.y + (max.h / 2) - (frame.h / 2)
 
-    win:setFrame(f)
+    win:setFrame(frame)
 end
 
 hookLeftMouseDragged = hs.eventtap.new({hs.eventtap.event.types.leftMouseDragged}, function(e)
     local location = e:location()
-    local screen = hs.mouse.getCurrentScreen()
-    local screenFrame = screen:frame()
+    local frame = hs.mouse.getCurrentScreen():frame()
 
-    if location.y <= screenFrame.y + 1.0 then
+    if location.y <= frame.y + 1.0 then
         hs.timer.doAfter(0.1, function()
             fill()
         end)
     end
 
-    if location.y <= screenFrame.y then
+    if location.y <= frame.y then
         hs.mouse.setRelativePosition({
             x = location.x,
             y = location.y
