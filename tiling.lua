b8461f9d16f60d98cf7ff28947493ab5055df191
Henry Li <yrnehli@proton.me>
2024-09-26 16:21:22 +0100

    multi monitor tiling

--- tiling.lua
+++ tiling.lua
@@ -13,27 +13,30 @@ end
 function shrink()
     local win = hs.window.focusedWindow()
     local f = win:frame()
-    local max = hs.screen.mainScreen():frame()
+    local screen = win:screen()
+    local max = screen:frame()
 
     f.w = max.w / 1.5
     f.h = max.h / 1.5
 
-    f.x = max.w / 2 - (f.w / 2)
-    f.y = max.h / 2 - (f.h / 2)
+    f.x = max.x + (max.w / 2) - (f.w / 2)
+    f.y = max.y + (max.h / 2) - (f.h / 2)
 
     win:setFrame(f)
 end
 
 hookLeftMouseDragged = hs.eventtap.new({hs.eventtap.event.types.leftMouseDragged}, function(e)
     local location = e:location()
+    local screen = hs.mouse.getCurrentScreen()
+    local screenFrame = screen:frame()
 
-    if location.y <= 1.0 then
+    if location.y <= screenFrame.y + 1.0 then
         hs.timer.doAfter(0.1, function()
             fill()
         end)
     end
 
-    if location.y == 0.0 then
+    if location.y <= screenFrame.y then
         hs.mouse.setRelativePosition({
             x = location.x,
             y = location.y
