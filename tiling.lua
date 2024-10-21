e93db1e1464903977d8e20d4d9547c72571cebfa
Henry Li <yrnehli@proton.me>
2024-10-21 22:01:27 +0100

    remove hack, not worth it

--- tiling.lua
+++ tiling.lua
@@ -25,45 +25,7 @@ function shrink()
     win:setFrame(frame)
 end
 
-leftMouseDragged = false
-
-hookLeftMouseDragged = hs.eventtap.new({hs.eventtap.event.types.leftMouseDragged}, function(e)
-    leftMouseDragged = true
-
-    local location = e:location()
-    local frame = hs.mouse.getCurrentScreen():fullFrame()
-
-    if location.y <= frame.y then
-        hs.mouse.setRelativePosition({
-            x = location.x,
-            y = location.y
-        }, "Hack to disable Mission Control")
-    end
-
-    hs.timer.doAfter(0.1, function()
-        leftMouseDragged = false
-    end)
-end)
-
-hookLeftMouseUp = hs.eventtap.new({hs.eventtap.event.types.leftMouseUp}, function(e)
-    if not leftMouseDragged then
-        return
-    end
-
-    local location = e:location()
-    local frame = hs.mouse.getCurrentScreen():fullFrame()
-
-    if location.y <= frame.y + 1.0 then
-        hs.timer.doAfter(0.1, function()
-            fill()
-        end)
-    end
-end)
-
 hs.hotkey.bind({"ctrl", "cmd"}, "w", fill)
 hs.hotkey.bind({"ctrl", "cmd"}, "a", left)
 hs.hotkey.bind({"ctrl", "cmd"}, "s", shrink)
 hs.hotkey.bind({"ctrl", "cmd"}, "d", right)
-
-hookLeftMouseDragged:start()
-hookLeftMouseUp:start()
