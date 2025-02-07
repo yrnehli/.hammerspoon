f77136c678aebbeb99436910032fd80c961f9039
Henry Li <henry@userled.io>
2025-02-07 10:48:15 +0000

    fix: hotkeys

--- hotkeys.lua
+++ hotkeys.lua
@@ -1,5 +1,30 @@
--- CMD+SHIFT+C => OPT+CMD+I
-hs.hotkey.bind({"cmd", "shift"}, "c", function()
-    hs.eventtap.event.newKeyEvent({"alt", "cmd"}, "i", true):post()
-    hs.eventtap.event.newKeyEvent({"alt", "cmd"}, "i", false):post()
-end)
+local chromeHotkey = nil
+
+function enableChromeHotkey()
+    if chromeHotkey == nil then
+        chromeHotkey = hs.hotkey.bind({"cmd", "shift"}, "c", function()
+            hs.eventtap.event.newKeyEvent({"alt", "cmd"}, "i", true):post()
+            hs.eventtap.event.newKeyEvent({"alt", "cmd"}, "i", false):post()
+        end)
+    end
+end
+
+function disableChromeHotkey()
+    if chromeHotkey ~= nil then
+        chromeHotkey:delete()
+        chromeHotkey = nil
+    end
+end
+
+function applicationWatcher(appName, eventType, appObject)
+    if (appName == "Google Chrome") then
+        if (eventType == hs.application.watcher.activated) then
+            enableChromeHotkey()
+        elseif (eventType == hs.application.watcher.deactivated) then
+            disableChromeHotkey()
+        end
+    end
+end
+
+hs.application.watcher.new(applicationWatcher):start()
+
